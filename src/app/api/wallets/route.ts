6ed73f40cd817b7a1b1520e81c74e9472b4ccc8f
Rahul Mishra <rahul07@Rahuls-Laptop.local>
2024-08-10 01:03:37 +0530

    added a wallet creationg function

---
+++ src/app/api/wallets/route.ts
@@ -0,0 +1,18 @@
+import { ethers } from "ethers";
+import { NextResponse } from "next/server";
+
+export async function GET(req: NextResponse) {
+    try {
+
+        const wallet = ethers.Wallet.createRandom()
+
+        return NextResponse.json({
+            Publickey: wallet.publicKey,
+            Adress: wallet.address,
+            Mnemonic: wallet.mnemonic?.phrase
+        })
+
+    } catch (error) {
+
+    }
+}
