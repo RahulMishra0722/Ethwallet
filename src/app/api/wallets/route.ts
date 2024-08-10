111eee25c63c312acf88bb93f313d6b8df63460c
Rahul Mishra <rahul07@Rahuls-Laptop.local>
2024-08-10 22:16:19 +0530

    added types

--- src/app/api/wallets/route.ts
+++ src/app/api/wallets/route.ts
@@ -1,19 +1,18 @@
 import { ethers } from "ethers";
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function GET(req: NextRequest) {
     try {
         const wallet = ethers.Wallet.createRandom();
-
         return NextResponse.json({
             Publickey: wallet.publicKey,
             Adress: wallet.address,
             Mnemonic: wallet.mnemonic?.phrase
-        });
+        }, { headers: { 'Cache-Control': 'no-store' } });
     } catch (error) {
         return NextResponse.json({
             message: 'Internal server error'
         });
     }
 }
-
